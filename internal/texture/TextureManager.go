477d2f273498b7239ffb6e16a16d5d6bf450d7bf
abieberbach <abieberbach@users.noreply.github.com>
2015-09-28 06:39:44 +0200

    Fehler beim asynchronen Laden der Texturen behoben

--- internal/texture/TextureManager.go
+++ internal/texture/TextureManager.go
@@ -17,29 +17,69 @@ import (
 	"path/filepath"
 	"github.com/abieberbach/goplanemp/gl"
 	"github.com/abieberbach/goplanemp/glu"
+	"github.com/abieberbach/goplane/extra/task"
+	"github.com/abieberbach/goplane/extra/logging"
 )
 
-var TextureManagerInstance = &TextureManager{make(map[string][2]int32), 0, false, false, -1, nil, false}
+var TextureManagerInstance = &TextureManager{make(map[string][2]int32), 0, "", false, false, -1, nil, false, task.NewTaskManager(10)}
 
 type TextureManager struct {
 	cache                map[string][2]int32
 	resolution           int
+	lightsFile           string
 	mergeLitTextures     bool
 	useAnyLitTexture     bool
 	lightsTextureNum     int32
 	nightLightingDataRef dataAccess.DataRef
 	texClampAvail        bool
+	taskManager          *task.TaskManager
 }
 
 func (self *TextureManager) Init(resolution int, lightsFile string, mergeLitTexture, useAnyLitTexture bool) (err error) {
 	self.resolution = resolution
 	self.mergeLitTextures = mergeLitTexture
 	self.useAnyLitTexture = useAnyLitTexture
-	self.lightsTextureNum, _, err = self.LoadTexture(lightsFile, true)
 	self.nightLightingDataRef, _ = dataAccess.FindDataRef("sim/graphics/scenery/percent_lights_on");
+	self.lightsFile = lightsFile
 	extensions := gl.GoStr(gl.GetString(gl.EXTENSIONS))
 	self.texClampAvail = strings.Contains(extensions, "GL_SGI_texture_edge_clamp") || strings.Contains(extensions, "GL_SGIS_texture_edge_clamp") || strings.Contains(extensions, "GL_ARB_texture_edge_clamp") || strings.Contains(extensions, "GL_EXT_texture_edge_clamp")
 	return
+}
+
+func (self *TextureManager) Enable() {
+	self.taskManager.Start()
+	//Textur für die Lichter asynchron laden, da die Tasks bein Plugin-Enable noch nicht ausgeführt werden
+	go func() {
+		var err error
+		self.lightsTextureNum, _, err = self.LoadTexture(self.lightsFile, true)
+		if err != nil {
+			logging.Errorf("could not load lights texture: %v", err)
+		}
+	}()
+}
+
+//Bindet die übergebene Textur an den OpenGL-Kontext.
+//Als data-Objekt ist die Textur als *image.NRGBA zu übergeben. Das Ergebnis ist die Textur-Nummer
+func bindTextureTaskFunc(data interface{}) (interface{}, error) {
+	textureNum := graphics.GenerateTextureNumbers(1)[0]
+	texture := data.(*image.NRGBA)
+	graphics.BindTexture2d(textureNum, 0)
+	size := texture.Rect.Size()
+	glu.Build2DMipmaps(gl.TEXTURE_2D, 4, size.X, size.Y, gl.RGBA, gl.UNSIGNED_BYTE, unsafe.Pointer(&texture.Pix[0]))
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST)
+	value := gl.CLAMP
+	if TextureManagerInstance.texClampAvail {
+		value = gl.CLAMP_TO_EDGE
+	}
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, int32(value))
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, int32(value))
+	errNum := gl.GetError()
+	if errNum > 0 {
+		return -1, errors.New(fmt.Sprintf("Texture load got OGL err: %v", errNum))
+	}
+	return textureNum, nil
+
 }
 
 func (self *TextureManager) GetLightTextureNumber() int32 {
@@ -49,7 +89,6 @@ func (self *TextureManager) GetLightTextureNumber() int32 {
 func (self *TextureManager) UseLitTexture() bool {
 	return self.nightLightingDataRef != nil&&dataAccess.GetFloatData(self.nightLightingDataRef) > 0.25
 }
-
 func (self *TextureManager) LoadTexture(textureFile string, forceMaxResolution bool) (int32, int32, error) {
 	textureNums, found := self.cache[textureFile]
 	if found {
@@ -62,8 +101,8 @@ func (self *TextureManager) LoadTexture(textureFile string, forceMaxResolution b
 		//Haupttextur konnte nicht geladen werden
 		return -1, -1, errors.New(fmt.Sprintf("could not load texture \"%v\": %v", textureFile, err))
 	}
-	textureNums = [2]int32{graphics.GenerateTextureNumbers(1)[0], -1}
-	err = self.bindTexture(textureNums[0], texImage)
+	textureNums = [2]int32{-1, -1}
+	textureNums[0], err = self.bindTexture(texImage)
 	if err != nil {
 		//Haupttextur konnte nicht gebunden werden
 		return -1, -1, errors.New(fmt.Sprintf("could not bind texture \"%v\": %v", textureFile, err))
@@ -79,16 +118,21 @@ func (self *TextureManager) LoadTexture(textureFile string, forceMaxResolution b
 			//Nachttextur mit der normalen Textur mischen, damit es Nachts besser aussieht
 			mergeTextures(texImage, litTexImage)
 		}
-		textureNums[1] = graphics.GenerateTextureNumbers(1)[0]
-		err = self.bindTexture(textureNums[1], litTexImage)
+		textureNums[1], err = self.bindTexture(litTexImage)
 		if err != nil {
 			//Nachttextur konnte nicht gebunden werden
 			return -1, -1, errors.New(fmt.Sprintf("could not bind lit texture \"%v\": %v", litTextureName, err))
 		}
 
 	}
-	self.cache[textureFile]=textureNums
+	self.cache[textureFile] = textureNums
 	return textureNums[0], textureNums[1], nil
+}
+
+func (self *TextureManager) bindTexture(texture *image.NRGBA) (int32, error) {
+	//Texturen müssen im X-Plane Hauptthread an den OpenGL-Kontext gebunden werden, da es sonst keine Texture-IDs gibt
+	result, err := self.taskManager.ExecuteTask(bindTextureTaskFunc, texture)
+	return result.(int32), err
 }
 
 func (self *TextureManager) loadTextureFromFile(texturePath string, isMagenta, forceMaxResolution bool) (*image.NRGBA, error) {
@@ -114,25 +158,6 @@ func (self *TextureManager) loadTextureFromFile(texturePath string, isMagenta, f
 		convertImageToAlpha(texture)
 	}
 	return texture, nil
-}
-
-func (self *TextureManager) bindTexture(textureNum int32, texture *image.NRGBA) error {
-	graphics.BindTexture2d(textureNum, 0)
-	size := texture.Rect.Size()
-	glu.Build2DMipmaps(gl.TEXTURE_2D, 4, size.X, size.Y, gl.RGBA, gl.UNSIGNED_BYTE, unsafe.Pointer(&texture.Pix[0]))
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST)
-	value := gl.CLAMP
-	if self.texClampAvail {
-		value = gl.CLAMP_TO_EDGE
-	}
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, int32(value))
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, int32(value))
-	err := gl.GetError()
-	if err > 0 {
-		return errors.New(fmt.Sprintf("Texture load got OGL err: %v", err))
-	}
-	return nil
 }
 
 func (self *TextureManager) findLitTexture(texturePath string) (string, bool) {
